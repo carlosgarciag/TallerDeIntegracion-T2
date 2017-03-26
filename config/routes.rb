71e4cd87e0e820085c830e251134a1e4c68a90a1
Carlos Garcia Galarce <cbgarcia1@uc.cl>
2017-03-26 09:51:30 -0300

    Vista inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :usuario, defaults: { format: :json }, :only => [:index, :show]
   put '/usuario' => 'usuario#create'
   delete '/usuario/:id' => 'usuario#destroy'
   post 'usuario/:id' => 'usuario#update'
+  
+  root "welcome#index"
 end
